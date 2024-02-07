a3148c5371258e97872ff9faba35581d00d374d5
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2024-02-08 00:22:04 +0600

    change hello_map.py

--- hello_map.py
+++ hello_map.py
@@ -1,35 +1,65 @@
-#!/usr/bin/python3  
+#!/usr/bin/python3
 from bcc import BPF
 from time import sleep
-program = r"""
-BPF_HASH(counter_table);
 
-int hello(void *ctx) {
-   u64 uid;
-   u64 counter = 0;
-   u64 *p;
+program_openat = r"""
+BPF_HASH(counter_table_openat);
 
-   uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-   p = counter_table.lookup(&uid);
-   if (p != 0) {
-      counter = *p;
-   }
-   counter++;
-   counter_table.update(&uid, &counter);
-   return 0;
+int hello_openat(void *ctx) {
+    u64 uid;
+    u64 counter = 0;
+    u64 *p;
+
+    uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    p = counter_table_openat.lookup(&uid);
+    if (p != 0) {
+        counter = *p;
+    }
+    counter++;
+    counter_table_openat.update(&uid, &counter);
+    return 0;
 }
 """
 
-b = BPF(text=program)
-syscall = b.get_syscall_fnname("execve")
-b.attach_kprobe(event=syscall, fn_name="hello")
+program_write = r"""
+BPF_HASH(counter_table_write);
 
-# Attach to a tracepoint that gets hit for all syscalls 
-# b.attach_raw_tracepoint(tp="sys_enter", fn_name="hello")
+int hello_write(void *ctx) {
+    u64 uid;
+    u64 counter = 0;
+    u64 *p;
+
+    uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
+    p = counter_table_write.lookup(&uid);
+    if (p != 0) {
+        counter = *p;
+    }
+    counter++;
+    counter_table_write.update(&uid, &counter);
+    return 0;
+}
+"""
+
+b = BPF(text=program_openat + program_write)
+
+syscall_openat = b.get_syscall_fnname("openat")
+syscall_write = b.get_syscall_fnname("write")
+
+b.attach_kprobe(event=syscall_openat, fn_name="hello_openat")
+b.attach_kprobe(event=syscall_write, fn_name="hello_write")
 
 while True:
     sleep(2)
     s = ""
-    for k,v in b["counter_table"].items():
+    
+    # Print openat counters
+    s += "openat: "
+    for k, v in b["counter_table_openat"].items():
         s += f"ID {k.value}: {v.value}\t"
+    
+    # Print write counters
+    s += "\nwrite: "
+    for k, v in b["counter_table_write"].items():
+        s += f"ID {k.value}: {v.value}\t"
+    
     print(s)
